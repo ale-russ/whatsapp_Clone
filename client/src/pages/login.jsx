fdea815d5d82c33f22f9e0ab97b8e8f8da229991
ale-russ-kabbee <arussom@kabbee.org>
2023-06-23 13:05:29 +0300

    context menu finished

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,21 +1,52 @@
+import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
 import { firebaseAuth } from "@/utils/FirebaseConfig";
+import axios from "axios";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
 function login() {
+  const router = useRouter();
+
+  const [{}, dispatch] = useStateProvider();
+
   const handleLogin = async () => {
-    // alert("login");
     const provider = new GoogleAuthProvider();
-    const { user } = await signInWithPopup(firebaseAuth, provider);
-    console.log("user: ", user);
+    const {
+      user: { displayName: name, email, photoURL: profileImage },
+    } = await signInWithPopup(firebaseAuth, provider);
+
+    try {
+      if (email) {
+        const { data } = await axios.post(CHECK_USER_ROUTE, { email });
+        console.log("Data: ", data);
+        if (!data.status) {
+          dispatch({ type: reducerCases.SET_NEW_USER, newUser: true });
+          dispatch({
+            type: reducerCases.SET_USER_INFO,
+            userInfo: {
+              name,
+              email,
+              profileImage,
+              status: "",
+            },
+          });
+          router.push("/onboarding");
+        }
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
   };
   return (
     <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col">
       <div className="flex items-center justify-center gap-2 text-white">
         <Image src="/whatsapp.gif" alt="whatsapp" width={300} height={300} />
-        <span className="text-7x">Whatsapp</span>
+        <span className="text-7xl">Whatsapp</span>
       </div>
       <button
         className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
